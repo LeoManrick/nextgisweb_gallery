eede71fbe07633a6afeb8a67891387e684e9b96f
LeoManrick <mariakot2008@gmail.com>
2024-08-02 01:59:26 +0000

    Display all the needed information for container

--- nextgisweb_gallery/nodepkg/gallery-display/GalleryDisplay.tsx
+++ nextgisweb_gallery/nodepkg/gallery-display/GalleryDisplay.tsx
@@ -5,6 +5,8 @@ import { useRouteGet } from "@nextgisweb/pyramid/hook";
 interface GalleryItem {
     id: number;
     title: string;
+    description: string;
+    resource_id: number;
 }
 
 export function GalleryDisplay({ id }: { id: number }) {
@@ -16,11 +18,20 @@ export function GalleryDisplay({ id }: { id: number }) {
 
     return (
         <ul>
-            {(data?.gallery?.items as GalleryItem[]).map(({ id, title }) => {
+            {(data?.gallery?.items as GalleryItem[]).map(({ id, title, description, resource_id }) => {
+                 const resourceLink = `http://localhost:8080/resource/${resource_id}`;
+
                 return (
                     <li key={id}>
                         <Space>
-                            {id} {title}
+                            <div><strong>Title:</strong> {title}</div>
+                            <div><strong>Description:</strong> {description}</div>
+                            <div>
+                                <strong>Link:</strong>
+                                <a href={resourceLink} target="_blank" rel="noopener noreferrer">
+                                    {resourceLink}
+                                </a>
+                            </div>
                         </Space>
                     </li>
                 );
@@ -28,3 +39,39 @@ export function GalleryDisplay({ id }: { id: number }) {
         </ul>
     );
 }
+
+
+// //To obtain all the availiable information about items:
+// interface GalleryDisplayProps {
+//     id: number;
+// }
+
+// export function GalleryDisplay({ id }: GalleryDisplayProps) {
+//     const { data, isLoading } = useRouteGet("resource.item", { id });
+
+//     if (isLoading) {
+//         return <LoadingWrapper />;
+//     }
+
+//     const items = data?.gallery?.items || [];
+
+//     return (
+//         <ul>
+//             {items.map((item: Record<string, any>, index: number) => {
+//                 return (
+//                     <li key={index}>
+//                         <Space direction="vertical">
+//                             {Object.entries(item).map(([key, value]) => {
+//                                 return (
+//                                     <div key={key}>
+//                                         <strong>{key}:</strong> {value}
+//                                     </div>
+//                                 );
+//                             })}
+//                         </Space>
+//                     </li>
+//                 );
+//             })}
+//         </ul>
+//     );
+// }
